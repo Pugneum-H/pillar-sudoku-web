c8d2a0fd1226d0f9747b4fe967a151ccbee81e05
G. Mh. <152092729+Pugneum-H@users.noreply.github.com>
2025-05-30 21:58:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import *
 from pillar_sudoku.pillar_sudoku import *
-from flask_wtf.csrf import CSRFProtect
-from flask_talisman import Talisman
+# from flask_wtf.csrf import CSRFProtect
+# from flask_talisman import Talisman
 
 
 
@@ -10,6 +10,8 @@ app = Flask(__name__)
 
 
 
+# Talisman(app)
+# csrf = CSRFProtect(app)
 
 orig = []
 grid = []
@@ -60,24 +62,22 @@ def main():
             sudoku.set_grid([chars_filter(i, fill_charset, 1)+" "*(1-len(i)) for i in request.values.getlist("fields")])
 
             grid = sudoku.get_grid()
-    
 
-    
+
+
     if request.method == 'GET':
         pass
     complete = sudoku.check()[1]
     correct = sudoku.check()[0]
 
 
-    return render_template('main.html', grid=sudoku.get_grid(), orig=orig, width=width, height=height, 
-                           fill_charset=fill_charset[:width*height], correct=correct, complete=complete, 
+    return render_template('main.html', grid=sudoku.get_grid(), orig=orig, width=width, height=height,
+                           fill_charset=fill_charset[:width*height], correct=correct, complete=complete,
                            size_selected=str(width)+str(height), difficulty_selected=str(ratio))
-    
+
 @app.route("/info")
 def info():
     return render_template("info.html")
 
-app.run()
-Talisman(app)
-csrf = CSRFProtect(app)
+
 
